fd07b36759b9e0201e633893528ac31d334a8f27
Kinkypeanut69 <balintharmse24@gmail.com>
2016-11-17 14:33:06 +0100

    fixed

--- Bomberman.ino
+++ Bomberman.ino
@@ -1,14 +1,6 @@
 void setup() {
     /* add setup code here */
-<<<<<<< HEAD
     /*werkt het???*/ 
-	/* jfoiapfeajfpoiwajfpoiwaoi */
-	/* hoi*/
-	/*more tests :)*/
-
-=======
-    /* werkt het??? */ 
->>>>>>> origin/master
 }
 
 void loop() {
