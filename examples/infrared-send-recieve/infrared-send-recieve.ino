dafc5b1dc9c12eb1d30bba34f42441964db02b9d
furkanpham <furkan.pham@gmail.com>
2016-11-18 14:06:42 +0100

    infrared pwm

--- examples/infrared-send-recieve/infrared-send-recieve.ino
+++ examples/infrared-send-recieve/infrared-send-recieve.ino
@@ -1,11 +1,31 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+uint16_t count = 0;
 int main(void) {
+    //    init();
+
     cli();
-    DDRB |= (1 << PINB3) | (1 << PINB0);
+    DDRB |= (1 << PINB3);
+    DDRB &= ~(1 << PINB0);
     TCCR2A = _BV(COM2A0) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-    TCCR2B |= (1 << WGM22) | (1 << CS20);
+    TCCR2B |= (1 << WGM22) | (1 << CS20); // prescaler 1024
     OCR2A = 210;
     sei();
+
+    Serial.begin(9600);
+    Serial.println(1);
+
+    for(;;) {
+        uint8_t rec;
+        rec = PINB & (1 << PINB0);
+        Serial.println(rec);
+    }
+}
+
+ISR(TIMER2_OVF_vect) {
+    count++;
+    if(count > 183) {
+        DDRB &= ~(1 << PINB3);
+    }
 }
