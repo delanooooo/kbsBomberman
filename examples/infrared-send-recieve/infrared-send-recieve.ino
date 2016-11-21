9993bd0cb06505e43fbcf5c1cfcfc43d7fe72877
furkanpham <furkan.pham@gmail.com>
2016-11-21 11:06:01 +0100

    .

--- examples/infrared-send-recieve/infrared-send-recieve.ino
+++ examples/infrared-send-recieve/infrared-send-recieve.ino
@@ -1,13 +1,18 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <Arduino.h>
+//#include <Arduino.h>
 
 uint16_t millisecond = 0;
 int main(void) {
     //    init();
 
     cli();
-    timer();
+	DDRB |= (1 << PINB3);
+	DDRB &= ~(1 << PINB0);
+	TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
+	TCCR2B |= (1 << WGM22) | (1 << CS20);
+	OCR2A = 210;
+    //timer();
     sei();
 
     Serial.begin(9600);
@@ -19,18 +24,13 @@ int main(void) {
         Serial.println(rec);
     }
 }
-
+/*
 ISR(TIMER2_OVF_vect) {
     millisecond++;
     if(millisecond > 26) {
         DDRB &= ~(1 << PINB3);
     }
 }
-
-int timer(){
-	DDRB |= (1 << PINB3);
-	DDRB &= ~(1 << PINB0);
-	TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
-	TCCR2B |= (1 << CS20);
-	OCR2A = 16;
-}
+*/
+//int timer(){
+//}
