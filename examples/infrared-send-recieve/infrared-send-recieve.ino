6201f97261d2d3cd92fa8113afd2ad4aaf8dfe51
furkanpham <furkan.pham@gmail.com>
2016-11-21 12:47:58 +0100

    .

--- examples/infrared-send-recieve/infrared-send-recieve.ino
+++ examples/infrared-send-recieve/infrared-send-recieve.ino
@@ -9,24 +9,28 @@ int main(void) {
     //    init();
 
     cli();
-	DDRB |= (1 << PINB3);
-	DDRB &= ~(1 << PINB0);
-	TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
-	TCCR2B |= (1 << WGM22) | (1 << CS20);
-	OCR2A = 210;
+    DDRB |= (1 << PINB3);
+    DDRB &= ~(1 << PINB0);
+    TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
+    TCCR2B |= (1 << WGM22) | (1 << CS20);
+    OCR2A = 210;
+    TIMSK2 |= (1 << OCIE2A);
     //timer();
     sei();
 
     Serial.begin(9600);
-    Serial.println(1);
+    Serial.println("Starting...");
 
     for(;;) {
-    //    uint8_t rec;
-     //   rec = PINB & (1 << PINB0);
-      //  Serial.println(rec);
+        //    uint8_t rec;
+        //   rec = PINB & (1 << PINB0);
+        //  Serial.println(rec);
 
-      sendZero();
-      _delay_ms(3000);
+        //_delay_ms(3000);
+        //sendZero();
+        Serial.println(time);
+        sendZero();
+    //    _delay_ms(3000);
     }
 }
 
@@ -35,9 +39,11 @@ ISR(TIMER2_COMPA_vect) {
 }
 
 void sendZero(){
-	DDRB ^= (1 << PINB3);
-	time = 0;
-	if(time == 3){
-		DDRB ^= (1 << PINB3);
-	}
+    DDRB ^= (1 << PINB3);
+    Serial.println("NO");
+    time = 0;
+    while(time < 3){
+        DDRB ^= (1 << PINB3);
+        Serial.println("YES");
+    }
 }
