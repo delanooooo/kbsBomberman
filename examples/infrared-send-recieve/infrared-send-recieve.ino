871c167f3ff0b5f6a4ca3a0b543c11eb04c77045
Kinkypeanut69 <balintharmse24@gmail.com>
2016-11-21 13:17:17 +0100

    comments

--- examples/infrared-send-recieve/infrared-send-recieve.ino
+++ examples/infrared-send-recieve/infrared-send-recieve.ino
@@ -6,31 +6,26 @@ void sendZero();
 
 uint16_t time = 0;
 int main(void) {
-    //    init();
-
     cli();
-    DDRB |= (1 << PINB3);
-    DDRB &= ~(1 << PINB0);
-    TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
-    TCCR2B |= (1 << WGM22) | (1 << CS20);
-    OCR2A = 210;
-    TIMSK2 |= (1 << OCIE2A);
-    //timer();
+    DDRB |= (1 << PINB3);													//output pin for LED
+    DDRB &= ~(1 << PINB0);													//input pin for IR sensor
+    TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);	
+    TCCR2B |= (1 << WGM22) | (1 << CS20);									
+    OCR2A = 210;															//approximately every 26 microseconds
+    TIMSK2 |= (1 << OCIE2A);												//enable timer compare match interupt
     sei();
 
-    Serial.begin(9600);
-    Serial.println("Starting...");
+//     Serial.begin(9600);
+//     Serial.println("Starting...");
 
     for(;;) {
         //    uint8_t rec;
         //   rec = PINB & (1 << PINB0);
         //  Serial.println(rec);
-
-        //_delay_ms(3000);
+/*        Serial.println(time);*/
         //sendZero();
-        Serial.println(time);
-        sendZero();
-    //    _delay_ms(3000);
+		//_delay_ms(3000);
+
     }
 }
 
@@ -38,12 +33,20 @@ ISR(TIMER2_COMPA_vect) {
     time++;
 }
 
-void sendZero(){
+void sendZero(){								//sends a zero by turning of the LED for approximately 60 microseconds
     DDRB ^= (1 << PINB3);
-    Serial.println("NO");
     time = 0;
     while(time < 3){
         DDRB ^= (1 << PINB3);
-        Serial.println("YES");
+/*        Serial.println("You sent a zero");*/
     }
+}
+	
+void sendOne(){									//sends a one by turning of the LED for approximately 120 microseconds
+	 DDRB ^= (1 << PINB3);
+	 time = 0;
+	 while(time < 6){
+		 DDRB ^= (1 << PINB3);
+/*		 Serial.println("You sent a one");*/
+	 }
 }
