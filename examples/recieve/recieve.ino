ed1a8d8535cdf87e66412847ecec28d863ef54dd
Kinkypeanut69 <balintharmse24@gmail.com>
2016-11-23 12:51:50 +0100

    bit checken

--- examples/recieve/recieve.ino
+++ examples/recieve/recieve.ino
@@ -2,30 +2,63 @@
 #include <util/delay.h>
 #include <Arduino.h>
 
+int measuredTime;
 int number = 0;
+uint8_t timer2 = 0;
+uint8_t timer = 0;
 uint8_t sensor = 0x00;
 int main(void){
 	own_init();
 	Serial.println("Starting...");
 	while(1){
-		Serial.print("");
-		if(sensor > 0){
-			number++;
-			Serial.print(number);
-			Serial.println("  SIGNAAAAl");	
+		Serial.print(timer);
+		if (measuredTime == 1){
+			Serial.println("0 Ontvangen");
+			led1();
+		} else if(measuredTime == 2){
+			Serial.println("1 Ontvangen");
+			led2();
 		}
 	}	
 }
 
-
+void led1(){
+	PORTB |= (1 << PINB2);
+	_delay_ms(500);
+	PORTB &= ~(1 << PINB2);
+}
+void led2(){
+	PORTB |= (1 << PINB1);
+	_delay_ms(500);
+	PORTB &= ~(1 << PINB1);
+}
 
 void own_init(){
 	sei();
+	DDRB |= (1 << PORTB2);
 	PCICR |= (1 << PCIE0);
 	PCMSK0 |= (1<< PCINT0);
 	EICRA = (1 << ISC11) | (1 << ISC10);
+	
+	TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
+	TCCR2B |= (1 << WGM22) | (1 << CS21);
+	OCR2A = 90; 
+	OCR2B = 45;
+	TIMSK2 = (1 << OCIE2A) | (1 << OCIE2B);
 	Serial.begin(9600);
 }
 ISR(PCINT0_vect){
 	sensor = PINB & (1<<PINB0);
+	timer = 0;
+	while(!sensor){
+		sensor = PINB & (1<<PINB0);
+	}
+	measuredTime = timer;
+	
 }
+ISR(TIMER2_COMPA_vect){
+	timer++;
+}
+// ISR(TIMER2_COMPB_vect){
+// 	timer2++;
+//}
