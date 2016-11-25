f26a1a36d564d6ceb5ee18d1b15951c21ad4b41d
Kinkypeanut69 <balintharmse24@gmail.com>
2016-11-25 13:22:53 +0100

    Can recieve and distinguish between 0 and 1

--- examples/recieve/recieve.ino
+++ examples/recieve/recieve.ino
@@ -6,7 +6,8 @@ void led1();
 void led2();
 void own_init();
 
-uint8_t measuredTime;
+uint8_t startMeasuring = 0x00;
+uint16_t measuredTime;
 uint8_t number = 0;
 uint8_t timer1 = 0;
 uint8_t sensor = 0x00;
@@ -16,17 +17,17 @@ int main(void){
     led2();
     Serial.println("Starting...");
     while(1){
-		if(sensor > 0){
-			timer1 = 0;
-			Serial.println(sensor);
-			while(!sensor) {
-				sensor = PINB & (1<<PINB0);
-			}
-			measuredTime = timer1;
-		}
-        if(measuredTime > 0) led1();
-        else led2();
-		measuredTime = 0;
+// 		if(sensor > 0){
+// 			timer1 = 0;
+// 			Serial.println(sensor);
+// 			while(!sensor) {
+// 				sensor = PINB & (1<<PINB0);
+// 			}
+// 			measuredTime = timer1;
+// 		}
+//         if(measuredTime > 0) led1();
+//         else led2();
+// 		measuredTime = 0;
     }
 }	
 
@@ -46,18 +47,30 @@ void own_init(){
     DDRB = (1 << PORTB2) | (1 << PORTB1);
     PCICR |= (1 << PCIE0);
     PCMSK0 |= (1<< PCINT0);
-    EICRA = (1 << ISC11) | (1 << ISC10);
+    EICRA = (1 << ISC11) | (1 << ISC00);
 
     TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
     TCCR2B |= (1 << WGM22) | (1 << CS21);
-    OCR2A = 30; 
+    OCR2A = 5; 
     TIMSK2 |= (1 << OCIE2A);
     Serial.begin(9600);
     sei();
 }
 ISR(PCINT0_vect){
     sensor = PINB & (1<<PINB0);
-    
+	if(sensor > 0){
+		timer1 = 0;
+	}
+	if(!sensor){
+		measuredTime = timer1;
+		if(measuredTime <= 101){
+			PORTB |= (1 << PINB1);
+			PORTB &= ~(1 << PINB2);
+			} else if(measuredTime > 101 && measuredTime < 160){
+			PORTB |= (1 << PINB2);
+			PORTB &= ~(1 << PINB1);
+		}
+	}
 }
 ISR(TIMER2_COMPA_vect){
     timer1++;
