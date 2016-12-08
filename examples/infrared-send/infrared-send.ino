c49689a913f0474b35362ac680f91c01d09de11a
furkanpham <furkan.pham@gmail.com>
2016-12-08 16:04:09 +0100

    added send byte

--- examples/infrared-send/infrared-send.ino
+++ examples/infrared-send/infrared-send.ino
@@ -2,53 +2,92 @@
 #include <util/delay.h>
 #include <Arduino.h>
 
-#define SWITCH 500
+#define SWITCH       5000
+#define START_SPACE   312
+#define STOP_SPACE    403
+#define ONE_SPACE     182
+#define ZERO_SPACE     52
+#define BIT_SPACE 494
 
+void sendStart();
+void sendStop();
 void sendZero();
 void sendOne();
 void ir_setup();
+void sendData(uint8_t data);
 
 volatile uint16_t sensor = 0x00;
 volatile uint16_t timer = 0;
 volatile uint16_t timer2 = 0;
-volatile uint16_t squarewave_timer = 0;
+volatile uint16_t spacetimer = 0;
 
 int main(void) {
-    Serial.begin(9600);
-    ir_setup();
-    for(;;) {
-		_delay_ms(SWITCH);
-        sendZero();
+        ir_setup();
+        _delay_ms(500);
+        PORTB ^= (1 << PINB5);
+        for(uint8_t i = 0x00; i < 0xFF; i++){
+            sendData(i);
+        }
         PORTB &= ~(1 << PINB5);
- 		_delay_ms(SWITCH);
- 		sendOne();
-        PORTB |= (1 << PINB5);
-    }
 }
 
 ISR(TIMER2_COMPA_vect) {
     timer++;
+    //    if(spacetimer > timer) DDRB |= (1 << PINB3);
 }
 
 ISR(TIMER2_COMPB_vect) {
     timer2++;
-    Serial.println(timer2);
-    if(squarewave_timer > timer2) DDRB |= (1 << PINB3);
 }
 
 ISR(PCINT0_vect) {
     sensor++;
 }
 
+void sendData(uint8_t data){
+    sendStart(); _delay_us(BIT_SPACE);
+    for(uint8_t mask = 1UL << 7;mask;mask >>= 1){
+        if(mask & data) {
+            sendOne();  _delay_us(BIT_SPACE);
+        } else { 
+            sendZero(); _delay_us(BIT_SPACE);
+        }
+    }
+    sendStop(); _delay_us(BIT_SPACE);
+}
+
+void sendBit(){
+    DDRB &= ~(1 << PINB3); // Turn IR led off
+    _delay_us(START_SPACE);
+    DDRB |= (1 << PINB3); // Turn IR led off
+}
+
+void sendStart(){                             
+    DDRB &= ~(1 << PINB3); // Turn IR led off
+    _delay_us(START_SPACE);
+    DDRB |= (1 << PINB3); // Turn IR led off
+    //    spacetimer = timer + ZERO_SPACE;
+}
+
+void sendStop(){                             
+    DDRB &= ~(1 << PINB3); // Turn IR led off
+    _delay_us(STOP_SPACE);
+    DDRB |= (1 << PINB3); // Turn IR led off
+    //    spacetimer = timer + ZERO_SPACE;
+}
+
 void sendZero(){                             
     DDRB &= ~(1 << PINB3); // Turn IR led off
-    squarewave_timer = timer2 + 5;
+    _delay_us(ZERO_SPACE);
+    DDRB |= (1 << PINB3); // Turn IR led off
+    //    spacetimer = timer + ZERO_SPACE;
 }
 
 void sendOne() {
-	DDRB &= ~(1 << PINB3); // Turn IR led off
-	_delay_us(182);
-	DDRB |= (1 << PINB3);
+    DDRB &= ~(1 << PINB3); // Turn IR led off
+    _delay_us(ONE_SPACE);
+    DDRB |= (1 << PINB3); // Turn IR led off
+    //    spacetimer = timer + ONE_SPACE;
 }
 
 void ir_setup() {
@@ -61,9 +100,9 @@ void ir_setup() {
     DDRB = (1 << PINB3) | (1 << PINB5); //output pin for LED
     DDRB &= ~(1 << PINB0); //input pin for IR sensor
     TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
-    TCCR2B |= (1 << WGM22) | (1 << CS21);
-    OCR2A = 0x1A; //approximately every 26 microseconds
-    OCR2B = 16; //approximately every 26 microseconds
+    TCCR2B |= (1 << WGM22) | (1 << CS20);
+    OCR2A = 210; //approximately every 26 microseconds
+    //   OCR2B = 16; //approximately every 16 microseconds
     TIMSK2 |= (1 << OCIE2A); //enable timer compare match interupt
     sei();
 }
