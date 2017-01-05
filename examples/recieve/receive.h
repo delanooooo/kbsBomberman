7fd3793ca9c7806ec19880de8bd1b28a5fd4242b
Kinkypeanut69 <balintharmse24@gmail.com>
2017-01-05 16:27:46 +0100

    Added receive.h for real

---
+++ examples/recieve/receive.h
@@ -0,0 +1,15 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <Arduino.h>
+
+
+//Functions
+void IR_setup();
+uint8_t readValueOld();
+uint8_t readValue();
+
+//Variables
+char data = 0;
+uint8_t startCollecting;
+volatile uint16_t measuredTime;
+volatile uint16_t timer = 0;
