df180dd622dabae0964860b6d0387ce6ce23172c
furkanpham <furkan.pham@gmail.com>
2017-01-06 11:19:24 +0100

    added queue to send and cleaned up code

--- examples/infrared-send/infrared-send.h
+++ examples/infrared-send/infrared-send.h
@@ -1,21 +1,26 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-// Variables
-volatile uint16_t sensor =      0x00;
-volatile uint16_t timer =       0x00;
-volatile uint16_t buffer =      0x00;
-volatile uint16_t sendtime =    0x00;
-volatile uint8_t  nbit =        0x00;
-volatile uint8_t  data =        0x00;
+//  Variables
+volatile uint16_t timer = 0;
+volatile uint16_t sendtime = 0;
 
-// Defines
-#define SWITCH       5000
-#define START_SPACE   312
-#define STOP_SPACE    416
-#define ONE_SPACE     234
-#define ZERO_SPACE     52
-#define BIT_SPACE     494
+volatile uint8_t  nbit = 0;
+volatile uint8_t  data = 0;
+
+//  Defines 
+//  Amount of microseconds set to send a specific signal
+#define START_SPACE    24 
+#define STOP_SPACE     32
+#define ONE_SPACE      18
+#define ZERO_SPACE      4
+#define BIT_SPACE      38
+
+#define SEND_START   sendtime = timer + START_SPACE
+#define SEND_STOP    sendtime = timer + STOP_SPACE
+#define SEND_BUFFER  sendtime = timer + BIT_SPACE
+#define SEND_ONE     sendtime = timer + ONE_SPACE
+#define SEND_ZERO    sendtime = timer + ZERO_SPACE
 
 #define IR_DISABLE DDRB &= ~(1 << PINB3)
 #define IR_ENABLE  DDRB |= (1 << PINB3)
@@ -23,127 +28,8 @@ volatile uint8_t  data =        0x00;
 #define PC_DISABLE DDRB &= ~(1 << PINB4)
 #define PC_ENABLE  DDRB |= (1 << PINB4)
 
+#define QUEUE_SIZE 100
+
 // Prototypes
 void ir_setup();
-void sendBit();
-void sendStart();            void sendStartOld();
-void sendStop();             void sendStopOld();
-void sendZero();             void sendZeroOld();
-void sendOne();              void sendOneOld();
-void sendData(uint8_t data); void sendDataOld(uint8_t data);
-
-ISR(TIMER2_COMPA_vect) {
-    timer++;
-//  if(timer > buffer)   IR_DISABLE;
-//  if(timer > sendtime) IR_ENABLE;
-}
-
-//void sendData(uint8_t d){
-//    timer = 0;
-//    nbit = (0x01 << 7);
-//    data = d;
-//    sendtime = timer + (START_SPACE / 13);
-//    buffer = sendtime + (BIT_SPACE / 13);
-//    IR_DISABLE;
-//}
-
-
-ISR(INT1_vect) {
-    DDRB |= (1 << PINB5);
-}
-
-void sendData(uint8_t data){
-    sendStart(); 
-    uint8_t mask = 0x01 << 7;
-    while(mask){
-        if(mask & data) sendOne();
-        else            sendZero();
-        mask >>= 1;
-        while(timer < buffer){}
-    }
-    sendStop();
-}
-
-void sendDataOld(uint8_t data){
-    sendStartOld(); _delay_us(BIT_SPACE);
-    for(uint8_t mask = 0x01 << 7; mask; mask >>= 1){
-        if(mask & data) {
-            sendOneOld();  _delay_us(BIT_SPACE);
-        } else { 
-            sendZeroOld(); _delay_us(BIT_SPACE);
-        }
-    }
-    sendStopOld(); _delay_us(BIT_SPACE);
-}
-
-void sendBit(){
-    IR_DISABLE;
-    sendtime = timer + (BIT_SPACE / 13);
-}
-
-void sendStart(){                             
-    sendtime = timer + (BIT_SPACE / 13);
-}
-
-void sendStartOld(){                             
-    IR_DISABLE;
-    _delay_us(START_SPACE);
-    IR_ENABLE;
-}
-
-void sendStop(){                             
-    buffer = timer + (STOP_SPACE / 13);
-    sendtime = buffer + (BIT_SPACE / 13);
-}
-
-void sendStopOld(){                             
-    DDRB &= ~(1 << PINB3); // Turn IR led off
-    _delay_us(STOP_SPACE);
-    DDRB |= (1 << PINB3); // Turn IR led off
-}
-
-void sendZero(){                             
-    buffer = timer + (ZERO_SPACE / 13);
-    sendtime = buffer + (BIT_SPACE / 13);
-}
-
-void sendZeroOld(){                             
-    DDRB &= ~(1 << PINB3); // Turn IR led off
-    _delay_us(ZERO_SPACE);
-    DDRB |= (1 << PINB3);
-}
-
-void sendOne() {
-    if(timer > sendtime){
-        timer = 0;
-        buffer = timer + (ONE_SPACE / 13);
-        sendtime = buffer + (BIT_SPACE / 13);
-    }
-}
-
-void sendOneOld() {
-    DDRB &= ~(1 << PINB3); // Turn IR led off
-    _delay_us(ONE_SPACE);
-    DDRB |= (1 << PINB3); // Turn IR led off
-}
-
-void ir_setup() {
-    cli();
-
-    PCICR |= (1 << PCIE2);   // Enable pin change interrupt 0
-    PCMSK0 |= (1 << PCINT0); // Listen to PINB4 for pin change interrupt
-    EICRA |= (1 << ISC10);
-    EIMSK |= (1 << INT1);
-
-    DDRB |= (1 << PINB5); 
-    DDRB |= (0 << PINB4); 
-    DDRB |= (1 << PINB3); //output pin for LED
-    DDRD |= (1 << PINB2);
-    DDRB &= ~(1 << PINB0); //input pin for IR sensor
-    TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
-    TCCR2B |= (1 << WGM22) | (1 << CS20);
-    OCR2A = 210; //approximately every 26 microseconds
-    //   OCR2B = 16; //approximately every 16 microseconds
-    TIMSK2 |= (1 << OCIE2A); //enable timer compare match interupt
-    sei();
-}
+void sendData(uint8_t data);
--- examples/infrared-send/infrared-send.ino
+++ examples/infrared-send/infrared-send.ino
@@ -1,11 +1,108 @@
 #include "infrared-send.h"
 
+typedef struct queueSend {
+    uint8_t data[QUEUE_SIZE];
+    uint8_t front;
+    uint8_t count;
+} queueSend;
+
+void queueAdd(queueSend *q, uint8_t d);
+void queueCheck(queueSend *q);
+
+queueSend *queue;
+
 int main(void) {
+    queue = (queueSend *) malloc(sizeof(queueSend));
+    queue->front = 0;
+    queue->count = 0;
+
     ir_setup();
 
-    PC_DISABLE;
+    IR_ENABLE;
+    PC_ENABLE;
+
+    for(uint8_t i=0;i<15;i++) sendData(0x41);
 
     for(;;){
+        queueCheck(queue);
+        _delay_ms(50);
+        queueAdd(queue, 0x37);
+        _delay_ms(50);
+        queueAdd(queue, 0x36);
+        _delay_ms(50);
     }
+}
 
+void queueAdd(queueSend *q, uint8_t d) {
+    if(q->count < QUEUE_SIZE) {
+        if((q->front + q->count) < QUEUE_SIZE) q->data[q->front + q->count] = d;
+        else q->data[q->front + q->count-QUEUE_SIZE] = d;
+    }
+    q->count++;
+}
+
+void queueCheck(queueSend *q) {
+    if(!data) {
+        sendData(q->data[q->front]);
+        q->data[q->front] = 0x57;
+        q->count--;
+        if(q->front < (QUEUE_SIZE-1)) q->front += 1;
+        else q->front = 0;
+    }
+}
+
+ISR(TIMER2_COMPA_vect) {
+    timer++;
+    if(timer > sendtime) {
+        PORTB ^= (1 << PINB4);
+    }
+}
+
+ISR(PCINT0_vect) {
+    if(data) {
+        if(PINB & (1 << PINB4)) {
+            IR_ENABLE;
+            SEND_BUFFER;
+            if(!nbit) data = 0x00;
+        }
+        else {
+            IR_DISABLE;
+            if(nbit) {
+                if(data & nbit) SEND_ONE;
+                else            SEND_ZERO;
+            } else              SEND_STOP;
+
+            nbit >>= 1;
+        }
+    }
+    else {
+        if(!(PINB & (1 << PINB4))) {
+            PORTB ^= (1 << PINB4);
+        }
+    }
+}
+
+void sendData(uint8_t d){
+    IR_DISABLE;
+    SEND_START;
+    data = d;
+    nbit = 0x80;
+}
+
+void ir_setup() {
+    cli();
+
+    PCICR |= (1 << PCIE0); // Enable pin change interrupt 0
+    PCMSK0 |= (1 << PCINT4); // Listen to PINB4 for pin change interrupt
+    EICRA |= (1 << ISC10);
+
+    DDRB |= (1 << PINB5); 
+    DDRB |= (0 << PINB4); 
+    DDRB |= (1 << PINB3); // infraredpin
+    TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
+    TCCR2B |= (1 << WGM22) | (1 << CS20);
+    OCR2A = 210; //approximately every 26 microseconds
+    //   OCR2B = 16; //approximately every 16 microseconds
+    TIMSK2 |= (1 << OCIE2A); //enable timer compare match interupt
+    sei();
 }
