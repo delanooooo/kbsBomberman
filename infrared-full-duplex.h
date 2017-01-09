8594cde5bb97c9e41fe36200e1324911ef84837e
furkanpham <furkan.pham@gmail.com>
2017-01-09 12:41:34 +0100

    spacing and indenting fix

--- infrared-full-duplex.h
+++ infrared-full-duplex.h
@@ -1,5 +1,4 @@
 #include <avr/io.h>
-#include <util/delay.h>
 
 extern volatile uint16_t datatimer;
 extern volatile uint16_t sendtime;
@@ -13,12 +12,12 @@ extern volatile uint8_t sentData;
 
 //  Defines
 //  Amount of cycles set to send a specific signal
-#define START_SPACE    65
-#define STOP_SPACE     90
-#define ONE_SPACE      40
-#define ZERO_SPACE     20
+#define START_SPACE  65
+#define STOP_SPACE   90
+#define ONE_SPACE    40
+#define ZERO_SPACE   20
 
-#define BIT_SPACE      20
+#define BIT_SPACE    20
 
 #define SEND_START   sendtime = datatimer + START_SPACE
 #define SEND_STOP    sendtime = datatimer + STOP_SPACE
@@ -26,13 +25,13 @@ extern volatile uint8_t sentData;
 #define SEND_ONE     sendtime = datatimer + ONE_SPACE
 #define SEND_ZERO    sendtime = datatimer + ZERO_SPACE
 
-#define IR_DISABLE DDRB &= ~(1 << PINB3)
-#define IR_ENABLE  DDRB |= (1 << PINB3)
+#define IR_DISABLE   DDRB &= ~(1 << PINB3)
+#define IR_ENABLE    DDRB |= (1 << PINB3)
 
-#define PC_DISABLE DDRB &= ~(1 << PINB4)
-#define PC_ENABLE  DDRB |= (1 << PINB4)
+#define PC_DISABLE   DDRB &= ~(1 << PINB4)
+#define PC_ENABLE    DDRB |= (1 << PINB4)
 
-#define QUEUE_SIZE 100
+#define QUEUE_SIZE   100
 
 // Prototypes
 void ir_setup();
