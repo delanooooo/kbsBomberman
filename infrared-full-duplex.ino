823213bdb7492c6c12c5147e60804ceb19eee819
GerbrandBoer <gerbrandboer@live.nl>
2017-01-10 15:28:07 +0100

    klopt

--- infrared-full-duplex.ino
+++ infrared-full-duplex.ino
@@ -156,6 +156,7 @@ void ir_setup(){
     /*Timer*/
     //listen for interrupts on compare match a
 	
+    TCCR2A = (0 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
 	TCCR2B |= (0 << WGM22) | (1 << CS20); //no prescaler 
     OCR2A = 210; //210 corresponds to 13 microseconds
     TIMSK2 |= (1 << OCIE2A); //enable datatimer compare match interupt
