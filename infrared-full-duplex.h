854002cbc0a2e9fb828e2e31ade4c12225f81646
furkanpham <furkan.pham@gmail.com>
2017-01-11 12:51:33 +0100

    code clean up

--- infrared-full-duplex.h
+++ infrared-full-duplex.h
@@ -2,39 +2,43 @@
 
 extern volatile uint16_t datatimer;
 extern volatile uint16_t sendtime;
+
 extern volatile uint16_t measuredTime;
 
-extern char receivedData;
+extern volatile uint8_t sentData;
+extern volatile uint8_t nbit;
+
+extern uint8_t receivedData;
 extern uint8_t startCollecting;
 
-extern volatile uint8_t nbit;
-extern volatile uint8_t sentData;
+/// Defines
+// pins used for sending data
+#define PIN_IR       PIND6
+#define PIN_PCINT    PIND2
 
-//  Defines
-//  Amount of (timer)cycles set to send a specific signal
+// amount of (timer)cycles set to send a specific signal
 #define START_SPACE  65
 #define STOP_SPACE   90
 #define ONE_SPACE    40
 #define ZERO_SPACE   20
-
 #define BIT_SPACE    20
 
-#define SEND_START   sendtime = datatimer + START_SPACE
-#define SEND_STOP    sendtime = datatimer + STOP_SPACE
-#define SEND_BUFFER  sendtime = datatimer + BIT_SPACE
-#define SEND_ONE     sendtime = datatimer + ONE_SPACE
-#define SEND_ZERO    sendtime = datatimer + ZERO_SPACE
+// defines used to start sending a specific type of signal
+#define SEND_START   sendtime = datatimer + START_SPACE     // start sending a start signal
+#define SEND_STOP    sendtime = datatimer + STOP_SPACE      // start sending a stop signal
+#define SEND_ONE     sendtime = datatimer + ONE_SPACE       // start sending a one bit
+#define SEND_ZERO    sendtime = datatimer + ZERO_SPACE      // start sending a zero bit
+#define SEND_BUFFER  sendtime = datatimer + BIT_SPACE       // used inbetween other send_spaces to be able to differentiate
 
-#define IR_DISABLE   DDRD &= ~(1 << PIND3)
-#define IR_ENABLE    DDRD |= (1 << PIND3)
+// enable/disable infrared led
+#define IR_DISABLE   DDRD &= ~(1 << PIN_IR)
+#define IR_ENABLE    DDRD |= (1 << PIN_IR)
+// enable/disable ability to trigger pin change interrupts
+#define PC_DISABLE   DDRD &= ~(1 << PIN_PCINT)  
+#define PC_ENABLE    DDRD |= (1 << PIN_PCINT)
 
-#define PC_DISABLE   DDRD &= ~(1 << PIND2)
-#define PC_ENABLE    DDRD |= (1 << PIND2)
+/// Prototypes
+void initIR();                  // set the correct registers to be able to use infrared
 
-#define QUEUE_SIZE   100
-
-// Prototypes
-void ir_setup();
-void sendData(uint8_t data);
-
-void readValue();
+void sendData(uint8_t data);    // send an 8bit byte
+void readValue();               // read data sent by another infrared led
--- infrared-full-duplex.ino
+++ infrared-full-duplex.ino
@@ -1,22 +1,20 @@
 #include "infrared-full-duplex.h"
 
-//  Variables
 volatile uint16_t datatimer = 0;
 volatile uint16_t sendtime = 0xFFFF;
 volatile uint16_t measuredTime = 0;
 
-char receivedData;
+uint8_t receivedData;
 uint8_t startCollecting;
-
 
 volatile uint8_t nbit = 0;
 volatile uint8_t sentData = 0;
 
-void sendData(uint8_t d){
-    IR_DISABLE;
-    SEND_START;
-    sentData = d;
-    nbit = 0x80;
+void sendData(uint8_t data){
+    IR_DISABLE;         //disable IR because we're going to start sending data
+    SEND_START;         //start out with a start signal
+    sentData = data;    //set byte we want to send
+    nbit = 0x80;        //used to determine what bit needs to be send
 }
 
 void readValue(){
@@ -28,6 +26,7 @@ void readValue(){
         startCollecting = 1;
         receivedData = 0;
     }
+
     if(startCollecting > 0){
         if(measuredTime >= 34 && measuredTime < 56){    //'1' bit
             receivedData <<= 1;     //shift in a new bit
@@ -41,36 +40,51 @@ void readValue(){
     }
 }
 
-// Interrupt service routines
 ISR(TIMER0_COMPA_vect){
-    datatimer++;
+    datatimer++;                    
     if(datatimer > sendtime) {
-        PORTD ^= (1 << PIND2);
+        //whenever a signal to send is set, the pin for pin change interrupts will toggle,
+        //which causes the INT0 interrupt service routine to be called
+        PORTD ^= (1 << PIN_PCINT);  
     }
 }
 
-ISR(INT0_vect) {
-        if(sentData) {
-        if(PIND & (1 << PIND2)) { 
-            IR_ENABLE; sendtime = 0xFFFF;
-            if(nbit) SEND_BUFFER;
-            else {
+//this interrupt triggers whenever a signal is done sending and is used
+//to determine which bit needs to be sent
+ISR(INT0_vect){
+
+    // only decide to check which signal needs to be sent if there's actually 
+    // still data left to be sent
+    if(sentData){
+
+        //if this interrupt is triggered while we were not sending a BUFFER signal 
+        if(PIND & (1 << PIN_PCINT)){
+            IR_ENABLE;
+
+            if(nbit) SEND_BUFFER; //if there are still bits left to send, continue to send buffer
+            else {                //if there aren't any bits left to send, prevent INT0 from being triggered again
                 sentData = 0x00;
                 sendtime = 0xFFFF;
             }
         }
+
+        // if this interrupt is triggerd while we were sending a BUFFER signal
         else {
             IR_DISABLE;
-            if(nbit) {
+
+            //if there are still bits left to send, send either a one or a zero
+            if(nbit){ 
                 if(sentData & nbit) SEND_ONE;
-                else            SEND_ZERO;
-            } else              SEND_STOP;
-            nbit >>= 1;
+                else SEND_ZERO;
+            //if there are not any bits left to send, send a stop signal
+            } else SEND_STOP; 
+
+            nbit >>= 1; //make sure the next INT0 trigger will check the next bit
         }
     }
 }
 
-//This interrupt triggers if there is any change coming from the sensor
+//this interrupt triggers if there is any change coming from the sensor
 ISR(PCINT2_vect){
     //check what state the sensor is in, rising or falling edge
     if(PIND & (1 << PIND4)){
@@ -91,26 +105,23 @@ ISR(PCINT2_vect){
     }
 }
 
-void ir_setup(){
+void initIR(){
     cli();
 
     //Pin change interrupt
     PCICR |= (1 << PCIE2); //pin group for PORTD
-    //EIMSK |= (1 << INT0); // Listen to PIND2 for pin change interrupt
     PCMSK2 |= (1 << PCINT20); //PIND4 / digital pin 4
-	
+
     EICRA |= (1 << ISC00); //create interrupt on any logical change
 
-    /*Timer*/
     //listen for interrupts on compare match a
-
     TCCR0A = (1 << COM0A0) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
     TCCR0B |= (1 << WGM02) | (1 << CS00); //no prescaler 
     OCR0A = 210; //210 corresponds to 13 microseconds
     TIMSK0 |= (1 << OCIE0A); //enable datatimer compare match interrupt
 
-    DDRD |= (1 << PIND2); // interrupt pin
-    DDRD |= (1 << PIND6); // infrared pin
+    DDRD |= (1 << PIN_PCINT); // interrupt pin
+    DDRD |= (1 << PIN_IR); // infrared pin
 
     sei();
 }
